890d94e3d761823feb1084e9a8680790df7b61ff
The-Dumpy <109386200+The-Dumpy@users.noreply.github.com>
2022-07-20 10:45:57 -0700

    Finished up program

--- Chapter 4 Beeg Number/Chapter 4 Beeg Number.cpp
+++ Chapter 4 Beeg Number/Chapter 4 Beeg Number.cpp
@@ -1,9 +1,24 @@
 
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    int arr[10];
+	int arr[10];
+	arr[0] = rand() % 1000 + 1;
+	int max = arr[0];
+	cout << "Array: " << arr[0];
+	for (int i = 1; i < sizeof(arr)/sizeof(int); i++)
+	{
+		arr[i] = rand() % 1000 + 1;
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+		cout << ", " << arr[i];
+	}
+	cout << endl;
+	cout << "Largest Number in Array is " << max;
+	exit(0);
 }
 
